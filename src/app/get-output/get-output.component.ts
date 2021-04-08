79a3ac1053115960714d439607f3606cdf60dc42
Edoardo <edoardo.roba@itauros.com>
2021-04-08 09:31:05 +0200

    updates dario

--- src/app/get-output/get-output.component.ts
+++ src/app/get-output/get-output.component.ts
@@ -17,27 +17,24 @@ export interface Config {
 })
 export class GetOutputComponent implements OnInit {
 
-  retrievedData : any //[] //Config = {
-  //   content: "string",
-  //   title: "string",
-  //   date: "any",
-  // }
+  retrievedData : Config[] = []
   items = []
   // retrievedData : any
 
   constructor(private http: HttpClient,private af:AngularFireDatabase) {}
 
   ngOnInit(): void {
-    this.retrievedData = this.http.get('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json')
-    // let obj = this.http.get('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json').toPromise().then((responseData) => {
-    //   // this.handleResponse(responseData);
-    //   console.log("data",responseData)
-    //   for (let key in responseData){
-    //     if (responseData.hasOwnProperty(key)){
-    //       this.items.push(responseData[key]);
-    //     }
-    //   }
-    // });
+    // this.retrievedData = this.http.get('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json')
+    let obj = this.http.get('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json').subscribe((responseData:Config[]) => {
+      // this.handleResponse(responseData);
+      this.retrievedData = responseData;
+      console.log("data: ",responseData)
+      // for (let key in responseData){
+      //   if (responseData.hasOwnProperty(key)){
+      //     this.items.push(responseData[key]);
+      //   }
+      // }
+    });
     // this.retrievedData = this.af.list('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json').snapshotChanges();
     // let obj = this.http.get<Config>('https://notes-c66a1-default-rtdb.firebaseio.com/notes.json').pipe(map((result: Config) => result));
     // console.log("obj",obj)
